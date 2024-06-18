ba3bb03302e3edf354b2cb93be4c4247aec6a49f
Uripro7 <160290059+Uripro7@users.noreply.github.com>
2024-06-18 17:23:13 +0000

    version1

---
+++ breakout/build/CMakeFiles/breakout.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/breakout.dir/main.cpp.o"
+  "breakout"
+  "breakout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
